c5c07209940d2d641b9593252b98e96676dfe21f
DARIA S <DARIA@EXAMPLE.COM>
2023-08-15 22:45:30 +0300

    add navmenu visibility

--- src/components/navmenu/nav.js
+++ src/components/navmenu/nav.js
@@ -1,15 +1,24 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import './nav.css';
+import { useState } from 'react';
 
 // eslint-disable-next-line import/prefer-default-export
 export function Navigation() {
+
+  const [visible, setVisible] = useState(false);
+  const toggleVisibility = () => setVisible(!visible);
+
     return (
        <div>
-                    <div className="nav__burger burger">
+            <div onClick = {toggleVisibility} className="nav__burger burger">
               <span className="burger__line" />
               <span className="burger__line" />
               <span className="burger__line" />
             </div>
+
+            {visible && (
             <div className="nav__menu menu">
               <ul className="menu__list">
                 <li className="menu__item">
@@ -23,6 +32,7 @@ export function Navigation() {
                 </li>
               </ul>
             </div>
-       </div>
+        )}
+        </div>
     )
 }
