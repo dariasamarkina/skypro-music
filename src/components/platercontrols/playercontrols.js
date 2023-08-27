7b6288024c2d839f9cbc1a5250c61ed2e6d2c254
DARIA S <DARIA@EXAMPLE.COM>
2023-08-28 00:42:27 +0300

    playercontrols

--- src/components/platercontrols/playercontrols.js
+++ src/components/platercontrols/playercontrols.js
@@ -1,36 +1,36 @@
-/* eslint-disable import/prefer-default-export */
-import './playercontrols.css';
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import * as S from './style';
 
+// eslint-disable-next-line import/prefer-default-export
 export function PlayerControls() {
   return (
-    <div className="player__controls">
-      <div className="player__btn-prev">
-        <svg className="player__btn-prev-svg" alt="prev">
+    <S.PlayerControls>
+      <S.PlayerButtonPrev>
+        <S.PlayerButtonPrevSvg alt="prev">
           <use xlinkHref="img/icon/sprite.svg#icon-prev" />
-        </svg>
-      </div>
-      <div className="player__btn-play _btn">
-        <svg className="player__btn-play-svg" alt="play">
+        </S.PlayerButtonPrevSvg>
+      </S.PlayerButtonPrev>
+      <S.PlayerButtonPlay>
+        <S.PlayerButtonPlaySvg alt="play">
           <use xlinkHref="img/icon/sprite.svg#icon-play" />
-        </svg>
-      </div>
-      <div className="player__btn-next">
-        <svg className="player__btn-next-svg" alt="next">
+        </S.PlayerButtonPlaySvg>
+      </S.PlayerButtonPlay>
+      <S.PlayerButtonPlayNext>
+        <S.PlayerButtonPlayNextSvg alt="next">
           <use xlinkHref="img/icon/sprite.svg#icon-next" />
-        </svg>
-      </div>
-      <div className="player__btn-repeat _btn-icon">
-        <svg className="player__btn-repeat-svg" alt="repeat">
+        </S.PlayerButtonPlayNextSvg>
+      </S.PlayerButtonPlayNext>
+      <S.PlayerButtonPlayRepeat>
+        <S.PlayerButtonPlayRepeatSvg alt="repeat">
           <use xlinkHref="img/icon/sprite.svg#icon-repeat" />
-        </svg>
-      </div>
-      <div className="player__btn-shuffle _btn-icon">
-        <svg className="player__btn-shuffle-svg" alt="shuffle">
+        </S.PlayerButtonPlayRepeatSvg>
+      </S.PlayerButtonPlayRepeat>
+      <S.PlayerButtonPlayShuffle>
+        <S.PlayerButtonPlayShuffleSvg alt="shuffle">
           <use xlinkHref="img/icon/sprite.svg#icon-shuffle" />
-        </svg>
-      </div>
-    </div>
+        </S.PlayerButtonPlayShuffleSvg>
+      </S.PlayerButtonPlayShuffle>
+    </S.PlayerControls>
   )
 }
