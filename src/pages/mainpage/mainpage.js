bdb1a25156fcb5c16b4db1c68cb677765f22529a
DARIA S <DARIA@EXAMPLE.COM>
2023-09-25 00:26:56 +0300

    get tracks from api

--- src/pages/mainpage/mainpage.js
+++ src/pages/mainpage/mainpage.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
 /* eslint-disable consistent-return */
 /* eslint-disable react/function-component-definition */
 /* eslint-disable import/prefer-default-export */
@@ -6,8 +8,19 @@ import { useState, useEffect } from 'react';
 import * as S from './styles';
 import { Main } from "../../components/main/main";
 import { Bar } from '../../components/bar/bar';
+import { getAllTracks } from '../../api';
 
 export const Mainpage = ({ setToken }) => {
+
+    const [getTracks, setGetTracks] = useState();
+    useEffect(() => {
+    // eslint-disable-next-line no-console
+    getAllTracks().then((tracks) => {
+        console.log(tracks);
+        setGetTracks(tracks);
+    });
+  }, [])
+
     const [isLoading, setIsLoading] = useState(true);
     useEffect(() => {
         const changeState = () => setIsLoading(!isLoading)
@@ -19,7 +32,7 @@ export const Mainpage = ({ setToken }) => {
     if (localStorage.getItem('token', 'token')) {
         return (
             <S.Container>
-                <Main isLoading={isLoading} setToken={setToken} />
+                <Main isLoading={isLoading} setToken={setToken} getTracks={getTracks} />
                 <Bar isLoading={isLoading}/>
             </S.Container>
         )
